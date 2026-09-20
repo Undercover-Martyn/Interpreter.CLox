a4c08b24ac82d11bc836903dee040fdfcac9bfdf
FurinaMyLove <ryan.chandra8308@gmail.com>
2026-09-20 21:16:21 +0530

    -added smth dk what i did add, will fix ts later

--- code_files/main.c
+++ code_files/main.c
@@ -1,38 +1,65 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+
 #include "../header_files/common.h"
 #include "../header_files/chunk.h"
 #include "../header_files/debug.h"
 #include "../header_files/vm.h"
+
+
+static void repl(){
+    char line[1024];
+    for(;;){
+        printf("clox> ");
+
+        if(!fgets(line,sizeof(line),stdin)){
+            printf("\n");
+            break;
+        }
+        interpret(line);
+    }
+}
+
+static void runFile(const char* path){
+    char* source = readFile(path);
+    InterpretResult result = interpret(source);
+    free(source);
+
+    if (result == INTERPRET_COMPILE_ERROR) exit(65);
+    if (result == INTERPRET_RUNTIME_ERROR) exit(65);
+}
+
+
+static char* readFile(const char* path){
+    FILE* file = fopen(path, "rb");
+    
+    fseek(file,0L,SEEK_END);
+    size_t fileSize = ftell(file);
+    rewind(file);
+
+    char* buffer = (char*)malloc(fileSize+1);
+    size_t bytesRead = fread(buffer,sizeof(char),fileSize,file);
+    buffer[bytesRead] = '\0';
+
+    fclose(file);
+    return buffer;
+}
+
 int main(int argc, const char *argv[])
 {
     initVM();
 
-    Chunk chunk;
-    initChunk(&chunk);
+    if (argc == 1){
+        repl();
+    }else if (argc == 2){
+        runfile(argv[1]);
+    }else{
+        fprintf(stderr, "Usage: clox [path]\n");
+        exit(64);
+    }
 
-    int constant = addConstant(&chunk, 1.2);
-
-    writeChunk(&chunk, OP_CONSTANT, 123);
-    writeChunk(&chunk, constant, 123);
-
-    constant = addConstant(&chunk, 3.4);
-    writeChunk(&chunk, OP_CONSTANT, 123);
-    writeChunk(&chunk, constant, 123);
-
-    writeChunk(&chunk, OP_ADD, 123);
-
-    constant = addConstant(&chunk, 5.6);
-    writeChunk(&chunk, OP_CONSTANT, 123);
-    writeChunk(&chunk, constant, 123);
-
-    writeChunk(&chunk, OP_DIVIDE, 123);
-
-    writeChunk(&chunk, OP_NEGATE, 123);
-
-    writeChunk(&chunk, OP_RETURN, 123);
-
-    disassembleChunk(&chunk, "test chunk");
-    interpret(&chunk);
     freeVM();
-    freeChunk(&chunk);
     return 0;
 }
